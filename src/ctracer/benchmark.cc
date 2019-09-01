87b9c511b29bd64f1a3de4f7fa5455f2158736f7
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-09-01 13:20:52 +0200

    benchmarking functions

---
+++ src/ctracer/benchmark.cc
@@ -0,0 +1,100 @@
+#include "benchmark.hh"
+
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+static std::string time_str(double s)
+{
+    std::stringstream ss;
+
+    if (s >= 1)
+        ss << std::setprecision(4) << s << " sec";
+    else if (s >= 1 / 1000.)
+        ss << std::setprecision(4) << s * 1000 << " ms";
+    else if (s >= 1 / 1000. / 1000.)
+        ss << std::setprecision(4) << s * 1000 * 1000 << " us";
+    else
+        ss << std::setprecision(4) << s * 1000 * 1000 * 1000 << " ns";
+
+    return ss.str();
+}
+
+static bool comp_by_seconds(ct::benchmark_results::timing const& a, ct::benchmark_results::timing const& b) { return a.seconds < b.seconds; }
+static bool comp_by_cycles(ct::benchmark_results::timing const& a, ct::benchmark_results::timing const& b) { return a.cycles < b.cycles; }
+
+void ct::benchmark_results::print_all(std::string_view prefix) const
+{
+    auto const print = [&](timing const& t) {
+        std::cout << prefix << "  " << t.cycles << " cycles, " << time_str(t.seconds) << ", " << t.samples << " sample(s)" << std::endl;
+    };
+
+    std::cout << prefix << "experiments:" << std::endl;
+    for (auto const& t : experiments)
+        print(t);
+    std::cout << prefix << "warmup:" << std::endl;
+    for (auto const& t : warmups)
+        print(t);
+    if (!baselines.empty())
+    {
+        std::cout << prefix << "baseline:" << std::endl;
+        for (auto const& t : baselines)
+            print(t);
+    }
+}
+
+void ct::benchmark_results::print_summary(std::string_view prefix) const
+{
+    auto const bsps = baseline_seconds_per_sample();
+    auto const bcps = baseline_cycles_per_sample();
+    auto const sps_min = seconds_per_sample() - bsps;
+    auto const cps_min = cycles_per_sample() - bcps;
+    auto const sps_max = seconds_per_sample(0.7);
+    auto const cps_max = cycles_per_sample(0.7);
+    std::cout << prefix << time_str(sps_min) << " .. " << time_str(sps_max) << " / sample, " << cps_min << " .. " << cps_max << " cycles / sample" << std::endl;
+}
+
+double ct::benchmark_results::seconds_per_sample(float percentile) const
+{
+    if (experiments.empty())
+        return -1;
+
+    auto const n = std::min(experiments.size() - 1, size_t(std::ceil(experiments.size() * percentile)));
+    auto exp = experiments; // copy
+    std::nth_element(exp.begin(), exp.begin() + n, exp.end(), comp_by_seconds);
+    auto const t = exp[n];
+    return t.seconds / t.samples;
+}
+
+double ct::benchmark_results::cycles_per_sample(float percentile) const
+{
+    if (experiments.empty())
+        return -1;
+
+    auto const n = std::min(experiments.size() - 1, size_t(std::ceil(experiments.size() * percentile)));
+    auto exp = experiments; // copy
+    std::nth_element(exp.begin(), exp.begin() + n, exp.end(), comp_by_cycles);
+    auto const t = exp[n];
+    return t.cycles / t.samples;
+}
+
+double ct::benchmark_results::baseline_seconds_per_sample() const
+{
+    if (baselines.empty())
+        return 0;
+
+    auto const t = std::min_element(baselines.begin(), baselines.end(), comp_by_seconds);
+    return t->seconds / t->samples;
+}
+
+double ct::benchmark_results::baseline_cycles_per_sample() const
+{
+    if (baselines.empty())
+        return 0;
+
+    auto const t = std::min_element(baselines.begin(), baselines.end(), comp_by_cycles);
+    return t->cycles / double(t->samples);
+}
---
+++ src/ctracer/benchmark.hh
@@ -0,0 +1,202 @@
+#pragma once
+
+#include <chrono>
+#include <limits>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
+#include <vector>
+
+#include "trace.hh"
+
+/*
+ * Benchmarking functionality
+ *
+ * Usage:
+ *   int foo(int) { ... }
+ *   auto res = ct::benchmark(foo, 1);
+ *   res.print_stats();
+ *
+ * How to prevent optimization:
+ *   ct::sink << x; // writes value to volatile var (guaranteed write)
+ *   auto const s = ct::source(0.0f);
+ *   float x = s; // reads 0.0f from a volatile var (guaranteed read)
+ *
+ *   (outputs are automatically passed to the sink)
+ *   (inputs are automatically read from a source)
+ *
+ *   Example:
+ *     auto s = ct::source(0.0f);
+ *     ct::benchmark([&] {
+ *        float a = s;
+ *        float b = s;
+ *        ct::sink << a + b;
+ *     });
+ *
+ * FIXME: gcc is misbehaving (https://godbolt.org/z/0HR2bL)
+ */
+
+namespace ct
+{
+struct sink_t
+{
+    template <class T>
+    void operator<<(T v) const
+    {
+        static volatile T s;
+        s = v;
+    }
+};
+
+static constexpr sink_t sink;
+
+template <class T>
+struct source
+{
+    explicit source(T v) : value(v) {}
+    operator T() const { return value; }
+
+private:
+    volatile T value;
+};
+
+struct benchmark_results
+{
+    struct timing
+    {
+        int samples = 0;
+        uint64_t cycles = 0;
+        double seconds = 0;
+    };
+
+    std::vector<timing> experiments;
+    std::vector<timing> warmups;
+    std::vector<timing> baselines;
+
+    void print_all(std::string_view prefix = "") const;
+    void print_summary(std::string_view prefix = "") const;
+
+    double seconds_per_sample(float percentile = 0.0f) const;
+    double cycles_per_sample(float percentile = 0.0f) const;
+    double baseline_seconds_per_sample() const;
+    double baseline_cycles_per_sample() const;
+};
+
+template <class F, class... Args>
+benchmark_results benchmark(F&& f, Args... args)
+{
+    static_assert(std::is_invocable_v<F, Args...>, "f is not invocable with the provided parameters");
+    using R = std::invoke_result_t<F, Args...>;
+    static_assert(std::is_default_constructible_v<R>, "requires default-constructible return type of f");
+
+    auto args_src = std::make_tuple(source<Args>(args)...);
+    auto args_in = std::make_tuple(args...);
+
+    auto constexpr initial_check_cnt = 3;
+    auto constexpr extra_long_cycles = 100'000'000; // above this only initial check is executed
+    auto constexpr long_cycles = 1'000'000;         // above this a few individual runs are performed
+    auto constexpr medium_cycles = 10000;           // above this a few clustered runs are performed
+    auto constexpr short_cycles = 500;              // above this a some clustered runs are performed
+
+    auto constexpr long_run_cnt = 5;
+    auto constexpr long_cluster_cnt = 1;
+
+    auto constexpr medium_run_cnt = 5;
+    auto constexpr medium_cluster_cnt = 5;
+
+    auto constexpr short_run_cnt = 10;
+    auto constexpr short_cluster_cnt = 100;
+
+    auto constexpr very_short_run_cnt = 10;
+    auto constexpr very_short_cluster_cnt = 1000;
+
+    auto constexpr baseline_run_cnt = 10;
+    auto constexpr baseline_cluster_cnt = 1000;
+
+    benchmark_results res;
+
+    auto const execute = [&] {
+        // read inputs from sources
+        args_in = args_src;
+
+        // execute function
+        if constexpr (std::is_same_v<R, void>)
+            std::apply(f, args_in);
+        else // sinking result
+            sink << std::apply(f, args_in);
+    };
+
+    auto const baseline = [&] {
+        args_in = args_src; // read
+        sink << R{};        // write
+    };
+
+    auto const time = [&](auto&& code, int count) -> benchmark_results::timing {
+        auto t_start = std::chrono::high_resolution_clock::now();
+        auto c_start = ct::current_cycles();
+        for (auto i = 0; i < count; ++i)
+            code();
+        auto c_end = ct::current_cycles();
+        auto t_end = std::chrono::high_resolution_clock::now();
+        return {count, c_end - c_start, std::chrono::duration<double>(t_end - t_start).count()};
+    };
+
+    benchmark_results::timing t_init;
+
+    // gauge function running time
+    {
+        auto c_min = std::numeric_limits<uint64_t>::max();
+        auto i_min = 0;
+        for (auto i = 0; i < initial_check_cnt; ++i)
+        {
+            auto t = time(execute, 1);
+            res.warmups.push_back(t);
+
+            if (t.cycles < c_min)
+            {
+                c_min = t.cycles;
+                i_min = i;
+            }
+        }
+        t_init = res.warmups[size_t(i_min)];
+    }
+
+    // function takes too long to do more than one run
+    if (t_init.cycles > extra_long_cycles)
+    {
+        res.experiments.push_back(t_init);
+    }
+    // a few individual runs are ok
+    else if (t_init.cycles > long_cycles)
+    {
+        res.experiments.push_back(t_init);
+        for (auto i = 0; i < long_run_cnt; ++i)
+            res.experiments.push_back(time(execute, long_cluster_cnt));
+    }
+    // a few clustered runs
+    else if (t_init.cycles > medium_cycles)
+    {
+        for (auto i = 0; i < medium_run_cnt; ++i)
+            res.experiments.push_back(time(execute, medium_cluster_cnt));
+    }
+    // some clustered runs
+    else if (t_init.cycles > short_cycles)
+    {
+        for (auto i = 0; i < short_run_cnt; ++i)
+            res.experiments.push_back(time(execute, short_cluster_cnt));
+    }
+    // heavily clustered runs
+    else
+    {
+        for (auto i = 0; i < very_short_run_cnt; ++i)
+            res.experiments.push_back(time(execute, very_short_cluster_cnt));
+    }
+
+    // baseline
+    for (auto i = 0; i < baseline_run_cnt; ++i)
+        res.baselines.push_back(time(baseline, baseline_cluster_cnt));
+
+    return res;
+}
+
+}
